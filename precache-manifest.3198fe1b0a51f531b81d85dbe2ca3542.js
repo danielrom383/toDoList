8aea709d407efb8d4fd1338c6d7742a1fe6626fd
danielrom383 <53815722+danielrom383@users.noreply.github.com>
2021-01-11 18:17:07 +0200

    first commit

---
+++ precache-manifest.3198fe1b0a51f531b81d85dbe2ca3542.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf7b7b10b5441118efaa82a418a30ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89ca79498b1a46bdd37a",
+    "url": "/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "f496c844d1ecea800663",
+    "url": "/static/css/main.98422c0e.chunk.css"
+  },
+  {
+    "revision": "89ca79498b1a46bdd37a",
+    "url": "/static/js/2.f1fb30d0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f1fb30d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f496c844d1ecea800663",
+    "url": "/static/js/main.6aa35aea.chunk.js"
+  },
+  {
+    "revision": "25b7cf8c0e59454f2987",
+    "url": "/static/js/runtime-main.6b3d1867.js"
+  }
+]);
